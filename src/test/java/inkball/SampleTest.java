0c7117b87975e5cd91324b92ac46a74fcd186ea4
Mahmoud Salah <58883594+susano254@users.noreply.github.com>
2024-12-21 19:59:27 +0200

    clean up

--- src/test/java/inkball/SampleTest.java
+++ src/test/java/inkball/SampleTest.java
@@ -15,11 +15,3 @@ public class SampleTest {
         app.delay(1000);*/ // delay is to give time to initialise stuff before drawing begins
     }
 }
-
-// gradle run						Run the program
-// gradle test						Run the testcases
-
-// Please ensure you leave comments in your testcases explaining what the testcase is testing.
-// Your mark will be based off the average of branches and instructions code coverage.
-// To run the testcases and generate the jacoco code coverage report: 
-// gradle test jacocoTestReport
